ca3947cafe6da7d96df64654d0dc99f33f8e1239
Anhquy23 <Ttm1022013@gmail.com>
2022-11-15 19:08:22 +0700

    Initial commit

---
+++ admin/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style_admin.css">
+</head>
+<body>
+    <div class="boxcenter">
+        <div class="row mb header">
+            <h1>ADMIN</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a href="index.php">Tranh chủ</a></li>
+                <li><a href="index.php?act=adddm">Danh mục</a></li>
+                <li><a href="index.php?act=addsp">Hàng hóa</a></li>
+                <li><a href="index.php?act=dskh">Khách hàng</a></li>
+                <li><a href="index.php?act=dsbl">Bình Luận</a></li>
+                <li><a href="index.php?act=thongke">Thông kê</a></li>
+            </ul>
+        </div>
---
+++ admin/index.php
@@ -0,0 +1,46 @@
+<?php
+    include "../model/pdo.php";
+    include "header.php";
+    // controller
+
+    if(isset($_GET['act'])){
+        $act = $_GET['act'];
+        switch ($act) {
+            case 'adddm':
+                //kiêm tra xem ng dùng có click vào nút add hay không
+                if(isset($_POST['themmoi'])&&($_POST['themmoi'])){
+                    $tenloai= $_POST['tenloai'];
+                    $sql = "INSERT INTO danhmuc(name) values('$tenloai')";
+                    pdo_execute($sql);
+                    $thongbao= "Thêm thành công";
+                }
+                include "danhmuc/add.php";
+                break;
+
+            case 'lisdm':
+                $sql = "select * from danhmuc order by id desc";
+                $listdanhmuc =pdo_query($sql);
+                include "danhmuc/list.php";
+                break;
+            
+            case 'xoadm':
+                if(isset($_GET['id'])&&($_GET['id']>0)){
+                    $sql = "delete from danhmuc where id =".$_GET['id'];
+                    pdo_execute($sql);
+                }
+                $sql = "select * from danhmuc order by id desc";
+                $listdanhmuc =pdo_query($sql);
+                include "danhmuc/list.php";
+                break;
+
+            default:
+                include "home.php";
+                break;
+        }
+    }else{
+        include "home.php";
+    }
+
+    include "footer.php";
+
+?>
